3f53d611215f2932687c9616fc1b9ebe1eed12e6
Bud Aminof <budaminof@gmail.com>
2019-03-25 18:41:54 -0600

    mocks(spec): added mock data

---
+++ test/__mocks__/data.js
@@ -0,0 +1,69 @@
+const data = [
+  {
+    userId: 1,
+    id: 5,
+    title: 'nesciunt quas odio',
+    body: 'repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque',
+  },
+  {
+    userId: 1,
+    id: 6,
+    title: 'dolorem eum magni eos aperiam quia',
+    body: 'ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae',
+  },
+  {
+    userId: 1,
+    id: 7,
+    title: 'magnam facilis autem',
+    body: 'dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas',
+  },
+  {
+    userId: 2,
+    id: 8,
+    title: 'dolorem dolore est ipsam',
+    body: 'dignissimos aperiam dolorem qui eum\nfacilis quibusdam animi sint suscipit qui sint possimus cum\nquaerat magni maiores excepturi\nipsam ut commodi dolor voluptatum modi aut vitae',
+  },
+  {
+    userId: 3,
+    id: 9,
+    title: 'nesciunt iure omnis dolorem tempora et accusantium',
+    body: 'consectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas',
+  },
+  {
+    userId: 4,
+    id: 10,
+    title: 'optio molestias id quia eum',
+    body: 'quo et expedita modi cum officia vel magni\ndoloribus qui repudiandae\nvero nisi sit\nquos veniam quod sed accusamus veritatis error',
+  },
+  {
+    userId: 4,
+    id: 11,
+    title: 'et ea vero quia laudantium autem',
+    body: 'delectus reiciendis molestiae occaecati non minima eveniet qui voluptatibus\naccusamus in eum beatae sit\nvel qui neque voluptates ut commodi qui incidunt\nut animi commodi',
+  },
+  {
+    userId: 4,
+    id: 12,
+    title: 'in quibusdam tempore odit est dolorem',
+    body: 'itaque id aut magnam\npraesentium quia et ea odit et ea voluptas et\nsapiente quia nihil amet occaecati quia id voluptatem\nincidunt ea est distinctio odio',
+  },
+  {
+    userId: 5,
+    id: 13,
+    title: 'dolorum ut in voluptas mollitia et saepe quo animi',
+    body: 'aut dicta possimus sint mollitia voluptas commodi quo doloremque\niste corrupti reiciendis voluptatem eius rerum\nsit cumque quod eligendi laborum minima\nperferendis recusandae assumenda consectetur porro architecto ipsum ipsam',
+  },
+  {
+    userId: 6,
+    id: 14,
+    title: 'voluptatem eligendi optio',
+    body: 'fuga et accusamus dolorum perferendis illo voluptas\nnon doloremque neque facere\nad qui dolorum molestiae beatae\nsed aut voluptas totam sit illum',
+  },
+  {
+    userId: 7,
+    id: 15,
+    title: 'eveniet quod temporibus',
+    body: 'reprehenderit quos placeat\nvelit minima officia dolores impedit repudiandae molestiae nam\nvoluptas recusandae quis delectus\nofficiis harum fugiat vitae',
+  },
+];
+export default data;
