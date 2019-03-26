776896475d4210b4a6bd6792eff176b0828f5430
Bud Aminof <budaminof@gmail.com>
2019-03-25 18:43:51 -0600

    Post Service (spec): unit test for the posts service method mocking axios call

--- src/services/PostService.js
+++ src/services/PostService.js
@@ -1,27 +1,8 @@
 import axios from 'axios';
 
-const apiClient = axios.create({
-  withCredentials: false,
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  },
-  timeout: 10000,
-  delayed: true,
-});
-
-// set delay:true to see the loading message
-apiClient.interceptors.request.use((config) => {
-  if (config.delayed) {
-    return new Promise(resolve => setTimeout(() => resolve(config), 4000));
-  }
-  return config;
-});
-
 export default {
   getPosts() {
-    return apiClient.get('https://jsonplaceholder.typicode.com/posts');
-    // thorw error to test the Error component
-    // throw new Error();
+    return axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => new Promise(resolve => setTimeout(() => resolve(res), 1000)));
   },
 };
